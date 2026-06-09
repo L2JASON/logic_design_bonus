b68000927a971c52b2c60edb64cef14dabe6984f
이신형 <leeshhy0613@naver.com>
2026-06-09 18:43:16 +0900

    pi개수가 가장 적은민텀을 찾음. 해당 pi에서 재귀구현중

--- src/search.cpp
+++ src/search.cpp
@@ -3,12 +3,44 @@
 
 #include "search.h"
 
+void recursive(Bits currentCovered, const ChartResult& chart){
+    int i, j, min = 64, minIndex;
+
+
+    for(i = 0; i<chart.uncoveredCols.size(); i++){
+        int currentMinterm = chart.uncoveredCols[i].mintermValue;
+
+        if (((currentCovered >> currentMinterm) & 1) == 1){
+            continue;
+        }
+
+        if (chart.uncoveredCols[i].coveredByRows.size() < min){
+            min = chart.uncoveredCols[i].coveredByRows.size();
+            minIndex = i;
+        }
+    }
+
+    if (minIndex == -1){
+        return;
+    }
+
+    for(j = 0; j<chart.uncoveredCols[minIndex].coveredByRows.size(); j++){
+        int piIndex = chart.uncoveredCols[minIndex].coveredByRows[j];
+
+        //수정중...
+        recursive(currentCovered, chart);
+    }
+
+    return;
+}
+
 std::vector<SOPCandidate> solveCyclicCore(
     const ChartResult& chart,
     int numVars
 ) {
     // TODO(이신형): 안 덮인 열 중 후보 PI가 가장 적은 열을 골라, 그 열을 덮는
     //              PI들로 분기하며 DFS. 최소 비용 조합(들)을 모아 반환.
-    (void)chart; (void)numVars;
-    return {};
+    Bits coveredValue = 0;
+
+    recursive(coveredValue, chart);
 }
